c4e159449c97b65b0847924246583dff3ea92238
Harshavardhan <harshkale612@gmail.com>
2026-01-08 14:17:33 +0530

    REST API

---
+++ 02-Rest-API/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let books = [
+  { id: "1", title: "Book 1" },
+  { id: "2", title: "Book 2" },
+  { id: "3", title: "Book 3" },
+];
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to our bookstore api",
+  });
+});
+
+app.get("/get", (req, res) => {
+  res.json(books);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
